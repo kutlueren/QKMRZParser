4f1e0bd87d79dd3f4befeb407f3c02e4bc663830
kutlueren <kutlu_eren26@hotmail.com>
2020-05-29 13:49:10 +0300

    string birthdate and expire date fields added
    string birthdate and expire date fields added

--- QKMRZParser/QKMRZResult.swift
+++ QKMRZParser/QKMRZResult.swift
@@ -8,21 +8,41 @@
 import Foundation
 
 public struct QKMRZResult {
+
     public let documentType: String
+
     public let countryCode: String
+
     public let surnames: String
+
     public let givenNames: String
+
     public let documentNumber: String
-    public let nationalityCountryCode: String
-    public let birthdate: Date? // `nil` if formatting failed
+
+    public let nationality: String
+
+    public let birthDate: Date? // `nil` if formatting failed
+
+    public let birthDateString: String
+
     public let sex: String? // `nil` if formatting failed
+
     public let expiryDate: Date? // `nil` if formatting failed
+
+    public let expiryDateString: String
+
     public let personalNumber: String
+
     public let personalNumber2: String? // `nil` if not provided
-    
+
     public let isDocumentNumberValid: Bool
-    public let isBirthdateValid: Bool
+
+    public let isBirthDateValid: Bool
+
     public let isExpiryDateValid: Bool
+
     public let isPersonalNumberValid: Bool?
+
     public let allCheckDigitsValid: Bool
+
 }
